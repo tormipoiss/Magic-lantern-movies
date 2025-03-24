3faa12af41d9a9dae0156be5fa87c6875340f507
tormipoiss <158473006+tormipoiss@users.noreply.github.com>
2025-03-24 14:45:30 +0200

    Added film and comment models

---
+++ Magic-lantern-movies/Magic-lantern-movies/Models/Film.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public enum Categories
+    {
+        Action,
+        Comedy,
+        Drama,
+        Horror,
+        SciFi,
+        Documentary,
+        Animation,
+        Adventure,
+        Fantasy,
+        Thriller
+    }
+    public enum AgeRatings
+    {
+        G,
+        PG,
+        PG_13,
+        R,
+        X
+    }
+    public enum Ratings
+    {
+        VeryNegative,
+        Negative,
+        Neutral,
+        Good,
+        VeryGood
+    }
+    public class Film
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Ratings Rating { get; set; }
+        public Categories Category { get; set; }
+        public IEnumerable<string> Actors { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public string OriginalLanguage { get; set; }
+        public string Director { get; set; }
+        public TimeSpan Duration { get; set; }
+        public AgeRatings AgeRating { get; set; }
+        public IEnumerable<Comment>? Comments { get; set; }
+    }
+}
