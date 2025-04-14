35c8f6d18087a7abb4dcd439984b89acb95da65c
Opilane TTHK <158561962+mel2134@users.noreply.github.com>
2025-04-14 17:11:21 +0300

    Deleted temporary home view, added fallback to the api and loading screen is now dark

--- Magic-lantern-movies/Services/MoviesService.cs
+++ Magic-lantern-movies/Services/MoviesService.cs
@@ -63,48 +63,47 @@ namespace Services
                 Debug.WriteLine($"Movies already exist");
                 return; // Skip if movies already exist
             }
-            var movies = new List<Movie>();
-            const string api_key = "91e0d4296bdfc99f07241e1b39b1f41f";
-            for(int i = 0; i <= 2; i++)
+            try
             {
-                var popularMoviesResp = await _httpClient.GetAsync($"https://api.themoviedb.org/3/movie/popular?page={i}&api_key=" + api_key);
-                if (popularMoviesResp.IsSuccessStatusCode)
+                var movies = new List<Movie>();
+                const string api_key = "91e0d4296bdfc99f07241e1b39b1f41f";
+                for (int i = 0; i <= 2; i++)
                 {
-                    var content = await popularMoviesResp.Content.ReadAsStringAsync();
-                    PopularMoviesJson data = JsonConvert.DeserializeObject<PopularMoviesJson>(content);
-                    foreach (MovieObj movieObj in data.Results)
+                    var popularMoviesResp = await _httpClient.GetAsync($"https://api.themoviedb.org/3/movie/popular?page={i}&api_key=" + api_key);
+                    if (popularMoviesResp.IsSuccessStatusCode)
                     {
-                        var movie = new Movie();
-                        var movieDetailsResp = await _httpClient.GetAsync($"https://api.themoviedb.org/3/movie/{movieObj.Id}?api_key=" + api_key);
-                        var detailsContent = await movieDetailsResp.Content.ReadAsStringAsync();
-                        JObject detailsData = JObject.Parse(detailsContent);
-                        movie.Categories = detailsData["genres"].Select(g => g["name"].ToString()).ToList();
-                        movie.Duration = TimeSpan.FromMinutes(detailsData["runtime"].ToObject<int>());
-                        movie.Name = movieObj.Title;
-                        movie.Description = movieObj.Overview;
-                        movie.Image = "https://image.tmdb.org/t/p/w500" + movieObj.Poster_path;
-                        movie.BackImage = "https://image.tmdb.org/t/p/w500" + movieObj.Backdrop_path;
-                        movie.OriginalLanguage = movieObj.Original_language;
-                        movie.Rating = await ParseRating(movieObj.Vote_average);
-                        movie.PublicationDate = DateTime.Parse(movieObj.Release_date.Replace("-", "/"));
-                        var result = await _databaseContext.SaveMovieAsync(movie);
-                        Debug.WriteLine($"Movie '{movie.Name}' saved with ID: {result}");
-                        await Task.Delay(200);
+                        var content = await popularMoviesResp.Content.ReadAsStringAsync();
+                        PopularMoviesJson data = JsonConvert.DeserializeObject<PopularMoviesJson>(content);
+                        foreach (MovieObj movieObj in data.Results)
+                        {
+                            var movie = new Movie();
+                            var movieDetailsResp = await _httpClient.GetAsync($"https://api.themoviedb.org/3/movie/{movieObj.Id}?api_key=" + api_key);
+                            var detailsContent = await movieDetailsResp.Content.ReadAsStringAsync();
+                            JObject detailsData = JObject.Parse(detailsContent);
+                            movie.Categories = detailsData["genres"].Select(g => g["name"].ToString()).ToList();
+                            movie.Duration = TimeSpan.FromMinutes(detailsData["runtime"].ToObject<int>());
+                            movie.Name = movieObj.Title;
+                            movie.Description = movieObj.Overview;
+                            movie.Image = "https://image.tmdb.org/t/p/w500" + movieObj.Poster_path;
+                            movie.BackImage = "https://image.tmdb.org/t/p/w500" + movieObj.Backdrop_path;
+                            movie.OriginalLanguage = movieObj.Original_language;
+                            movie.Rating = await ParseRating(movieObj.Vote_average);
+                            movie.PublicationDate = DateTime.Parse(movieObj.Release_date.Replace("-", "/"));
+                            var result = await _databaseContext.SaveMovieAsync(movie);
+                            Debug.WriteLine($"Movie '{movie.Name}' saved with ID: {result}");
+                            await Task.Delay(200);
 
+                        }
                     }
                 }
             }
-            /*
-            try
+            catch
             {
-                var existingMovies = await _databaseContext.GetMoviesAsync().ConfigureAwait(false);
-                if (existingMovies.Any()) 
+                Debug.WriteLine("Failed to reach api! Using preset data..");
+                try
                 {
-                    Debug.WriteLine($"Movies already exist");
-                    return; // Skip if movies already exist
-                } 
 
-                var movies = new List<Models.Movie>
+                    var movies = new List<Movie>
                 {
                     new()
                     {
@@ -112,13 +111,11 @@ namespace Services
                         Description = "A banker convicted of uxoricide forms a friendship over a quarter century with a hardened convict, while maintaining his innocence and trying to remain hopeful through simple compassion.",
                         Rating = Ratings.VeryGood,
                         Image = "https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg",
-                        Categories = new() { Categories.Drama },
-                        Actors = new() { "Tim Robbins", "Morgan Freeman", "Bob Gunton" },
+                        Categories = new() { "Drama" },
                         PublicationDate = new DateTime(1994, 9, 10),
                         OriginalLanguage = "English",
                         Directors = new() { "Frank Darabont" },
                         Duration = new TimeSpan(2, 22, 0),
-                        AgeRating = AgeRatings.R,
                     },
                     new()
                     {
@@ -126,13 +123,11 @@ namespace Services
                         Description = "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
                         Rating = Ratings.VeryGood,
                         Image = "https://upload.wikimedia.org/wikipedia/en/1/1c/Godfather_ver1.jpg",
-                        Categories = new() { Categories.Drama, Categories.Tragedy, Categories.Crime },
-                        Actors = new() { "Marlon Brando", "Al Pacino", "James Caan" },
+                        Categories = new() {"Drama","Tragedy","Crime"},
                         PublicationDate = new DateTime(1972, 3, 14),
                         OriginalLanguage = "English",
                         Directors = new() { "Francis Ford Coppola" },
                         Duration = new TimeSpan(2, 55, 0),
-                        AgeRating = AgeRatings.R,
                     },
                     new()
                     {
@@ -140,13 +135,11 @@ namespace Services
                         Description = "When a menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman, James Gordon and Harvey Dent must work together to put an end to the madness.",
                         Rating = Ratings.VeryGood,
                         Image = "https://upload.wikimedia.org/wikipedia/en/1/1c/The_Dark_Knight_%282008_film%29.jpg",
-                        Categories = new() { Categories.Action, Categories.Tragedy, Categories.Superhero, Categories.Epic },
-                        Actors = new() { "Christian Bale", "Heath Ledger", "Aaron Eckhart" },
+                        Categories = new() {"Action","Tragedy","Superhero","Epic"},
                         PublicationDate = new DateTime(2008, 7, 18),
                         OriginalLanguage = "English",
                         Directors = new() { "Christopher Nolan" },
                         Duration = new TimeSpan(2, 32, 0),
-                        AgeRating = AgeRatings.PG_13,
                     },
                     new()
                     {
@@ -154,13 +147,11 @@ namespace Services
                         Description = "Over the course of one evening, an unsuspecting group of twenty-somethings find themselves bombarded by a series of natural disasters and catastrophic events.",
                         Rating = Ratings.VeryNegative,
                         Image = "https://upload.wikimedia.org/wikipedia/en/a/af/Disaster_movie.jpg",
-                        Categories = new() { Categories.Disaster, Categories.Parody, Categories.Superhero },
-                        Actors = new() { "Carmen Electra", "Vanessa Lachey", "Nichole Parker" },
+                        Categories = new() {"Disaster","Parody","Superhero"},
                         PublicationDate = new DateTime(2008, 8, 29),
                         OriginalLanguage = "English",
                         Directors = new() { "Jason Friedberg", "Aaron Seltzer" },
                         Duration = new TimeSpan(1, 27, 0),
-                        AgeRating = AgeRatings.PG_13,
                     },
                     new()
                     {
@@ -168,13 +159,11 @@ namespace Services
                         Description = "A spoof on previous years' epic movies (The Da Vinci Code (2006), The Chronicles of Narnia: The Lion, the Witch and the Wardrobe (2005) + 20 more), TV series, music videos and celebs. 4 orphans are on an epic adventure.",
                         Rating = Ratings.Negative,
                         Image = "https://upload.wikimedia.org/wikipedia/en/0/02/Epicmovieposter.jpg",
-                        Categories = new() { Categories.Adventure, Categories.Parody, Categories.Comedy },
-                        Actors = new() { "Kal Penn", "Jennifer Coolidge", "Fred Willard" },
+                        Categories = new() {"Adventure","Parody","Comedy" },
                         PublicationDate = new DateTime(2007, 1, 26),
                         OriginalLanguage = "English",
                         Directors = new() { "Jason Friedberg", "Aaron Seltzer" },
                         Duration = new TimeSpan(1, 26, 0),
-                        AgeRating = AgeRatings.PG_13,
                     },
                     new()
                     {
@@ -182,13 +171,11 @@ namespace Services
                         Description = "Four misfits are suddenly pulled through a mysterious portal into a bizarre, cubic wonderland that thrives on imagination. To get back home, they'll have to master this world while embarking on a quest with an unexpected, expert crafter.",
                         Rating = Ratings.Good,
                         Image = "https://upload.wikimedia.org/wikipedia/en/6/66/A_Minecraft_Movie_poster.jpg",
-                        Categories = new() { Categories.Action, Categories.Epic},
-                        Actors = new() { "Jack Black", "Jason Momoa", "Sebastian Hansen" },
+                        Categories = new() {"Action","Epic"},
                         PublicationDate = new DateTime(2025, 4, 4),
                         OriginalLanguage = "English",
                         Directors = new() { "Jared Hess" },
                         Duration = new TimeSpan(1, 41, 0),
-                        AgeRating = AgeRatings.PG,
                     },
                     new()
                     {
@@ -196,13 +183,11 @@ namespace Services
                         Description = "Andres settles in a new farm. With rose-tinted lenses, he sets out to transform the farm into a prosperous paradise - all he needs is a healthy harvest, cooperative neighbors and a son. But, life turns out to be cruel and treacherous.",
                         Rating = Ratings.VeryGood,
                         Image = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/TODE_JA_OIGUS_p%C3%B5hiposter.jpg/330px-TODE_JA_OIGUS_p%C3%B5hiposter.jpg",
-                        Categories = new() { Categories.Drama },
-                        Actors = new() { "Priit Loog", "Ester Kuntu", "Priit Võigemast" },
+                        Categories = new() {"Drama"},
                         PublicationDate = new DateTime(2019, 2, 22),
                         OriginalLanguage = "Estonian",
                         Directors = new() { "Tanel Toom" },
                         Duration = new TimeSpan(2, 45, 0),
-                        AgeRating = AgeRatings.PG_13,
                     },
                     new()
                     {
@@ -210,13 +195,11 @@ namespace Services
                         Description = "After Christopher Robin abandons them for college, Pooh and Piglet embark on a bloody rampage as they search for a new source of food.",
                         Rating = Ratings.Negative,
                         Image = "https://upload.wikimedia.org/wikipedia/en/7/74/Winnie_the_Pooh%2C_Blood_and_Honey_Film_Poster.jpg",
-                        Categories = new() { Categories.Horror, Categories.Monster },
-                        Actors = new() { "Nikolai Leon", "Maria Taylor", "Natasha Rose Mills" },
+                        Categories = new() {"Horror","Monster" },
                         PublicationDate = new DateTime(2023, 3, 17),
                         OriginalLanguage = "English",
                         Directors = new() { "Rhys Frake-Waterfield" },
                         Duration = new TimeSpan(1, 24, 0),
-                        AgeRating = AgeRatings.Not_Rated,
                     },
                     new()
                     {
@@ -224,27 +207,25 @@ namespace Services
                         Description = "On the way to the biggest race of his life, a hotshot rookie race car gets stranded in a rundown town and learns that winning isn't everything in life.",
                         Rating = Ratings.Good,
                         Image = "https://upload.wikimedia.org/wikipedia/en/3/34/Cars_2006.jpg",
-                        Categories = new() { Categories.Comedy, Categories.Animation, Categories.Motorsport, Categories.Adventure, Categories.Family },
-                        Actors = new() { "Owen Wilson", "Bonnie Hunt", "Paul Newman" },
+                        Categories = new() {"Animation","Comedy","Family","Motorsport","Adventure"},
                         PublicationDate = new DateTime(2006, 6, 9),
                         OriginalLanguage = "English",
                         Directors = new() { "John Lasseter", "Joe Ranft" },
                         Duration = new TimeSpan(1, 56, 0),
-                        AgeRating = AgeRatings.G,
                     }
                 };
 
-                foreach (var movie in movies)
+                    foreach (var movie in movies)
+                    {
+                        var result = await _databaseContext.SaveMovieAsync(movie).ConfigureAwait(false);
+                        Debug.WriteLine($"Movie '{movie.Name}' saved with ID: {result}");
+                    }
+                }
+                catch (Exception ex)
                 {
-                    var result = await _databaseContext.SaveMovieAsync(movie).ConfigureAwait(false);
-                    Debug.WriteLine($"Movie '{movie.Name}' saved with ID: {result}");
+                    Debug.WriteLine($"Error initializing movies: {ex.Message}");
                 }
             }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"Error initializing movies: {ex.Message}");
-            }
-            */
         }
     }
 }
