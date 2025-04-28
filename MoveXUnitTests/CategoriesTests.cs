285c1391c3f00ad7ca72f344369ae2ad6b176c1c
cristian449 <144561600+cristian449@users.noreply.github.com>
2025-04-28 16:08:43 +0300

    Replaced all the old tests in the Categories tests as they werent all that relevant to the exisiting code so updated them with much more complicated tests that should work

--- MoveXUnitTests/CategoriesTests.cs
+++ MoveXUnitTests/CategoriesTests.cs
@@ -3,75 +3,80 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Data;
 using Magic_lantern_movies;
 using Models;
+using ViewModels;
 
 namespace MoveXUnitTests
 {
-    public class CategoriesTests
+    
+
+    public class CategoriesViewModelTests
     {
-        [Fact]
-        public void AddCategory_ShouldAddcategoryToList()
+        private readonly DatabaseContext _mockDb;
+
+        public CategoriesViewModelTests()
         {
-            // Arrangee
-
-            var categories = new List<string>();
-            var categoryToAdd = "Sports";
-
-            // ACT
-
-            categories.Add(categoryToAdd);
-
-            // Assert
-
-            Assert.Contains(categoryToAdd, categories);
+            _mockDb = new DatabaseContext("mockDbPath");
         }
 
         [Fact]
-        public void RemoveCategory_ShouldremoveCategoryFromList()
+        public async Task InitializeAsync_ShouldLoadCategories()
         {
             // Arrange
+            var viewModel = new CategoriesViewModel(_mockDb);
 
-            var categories = new List<string> { "Sports", "Music", "Movies" };
-            var categoryToRemove = "Music";
-
-            // Acct
-
-            categories.Remove(categoryToRemove);
-
-            // Asserty
-
-            Assert.DoesNotContain(categoryToRemove, categories);
-        }
+            // Act
+            await viewModel.InitializeAsync();
 
 
-        [Fact]
-        public void ClearCategories_ShouldemptyThelist()
-        {
-            // Arrange
-
-            var categories = new List<string> { "Sports", "Music", "Movies" };
-
-            // ACT
-
-            categories.Clear();
 
             // Assert
-            Assert.Empty(categories);
+            Assert.NotEmpty(viewModel.Categories);
+            Assert.Contains("Action", viewModel.Categories); 
         }
 
-
         [Fact]
-        public void IsCategoryEmpty_ShouldReturnTrueForEmptyList()
+        public void UpdateSpan_ShouldSetColumnSpanBasedOnWidth()
         {
             // Arrange
-            var categories = new List<string>();
+            var viewModel = new CategoriesViewModel(_mockDb);
 
             // ACT
-            var isEmpty = categories.Count == 0;
+            viewModel.UpdateSpan(800);
 
             // Assert
-            Assert.True(isEmpty);
+            Assert.Equal(2, viewModel.ColumnSpan);
+
+            // ACT
+            viewModel.UpdateSpan(500); 
+
+            // Assert
+            Assert.Equal(1, viewModel.ColumnSpan);
+        }
+
+        [Fact]
+        public async Task GoTo_ShouldNavigateToMoviesByCategoryPage()
+        {
+            // Arrange
+            var viewModel = new CategoriesViewModel(_mockDb);
+            var category = "Action";
+
+            // Act
+            await viewModel.GoToCommand.ExecuteAsync(category);
+
+            Assert.True(true); // Placeholder assertion
+        }
+
+        [Fact]
+        public void Categories_ShouldBeEmptyInitially()
+        {
+            // Arrange
+            var viewModel = new CategoriesViewModel(_mockDb);
+
+            // Assert
+            Assert.Empty(viewModel.Categories);
         }
     }
 }
