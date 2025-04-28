e407b217d276b25eaee655aed97fb6139a6c1b24
cristian449 <144561600+cristian449@users.noreply.github.com>
2025-04-28 15:06:21 +0300

    Removed redundant files that for some reason remained, finished test for categories, tests adding, removing and clearing the list

---
+++ MoveXUnitTests/CategoriesTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Magic_lantern_movies;
+using Models;
+
+namespace MoveXUnitTests
+{
+    public class CategoriesTests
+    {
+        [Fact]
+        public void AddCategory_ShouldAddcategoryToList()
+        {
+            // Arrangee
+
+            var categories = new List<string>();
+            var categoryToAdd = "Sports";
+
+            // ACT
+
+            categories.Add(categoryToAdd);
+
+            // Assert
+
+            Assert.Contains(categoryToAdd, categories);
+        }
+
+        [Fact]
+        public void RemoveCategory_ShouldremoveCategoryFromList()
+        {
+            // Arrange
+
+            var categories = new List<string> { "Sports", "Music", "Movies" };
+            var categoryToRemove = "Music";
+
+            // Acct
+
+            categories.Remove(categoryToRemove);
+
+            // Asserty
+
+            Assert.DoesNotContain(categoryToRemove, categories);
+        }
+
+
+        [Fact]
+        public void ClearCategories_ShouldemptyThelist()
+        {
+            // Arrange
+
+            var categories = new List<string> { "Sports", "Music", "Movies" };
+
+            // ACT
+
+            categories.Clear();
+
+            // Assert
+            Assert.Empty(categories);
+        }
+    }
+}
